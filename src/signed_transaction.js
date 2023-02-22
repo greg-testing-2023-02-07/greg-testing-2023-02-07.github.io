640973ad8888ba322a9510c0b4ed3fc69ed8218b
Gregory Hale <imalsogreg@gmail.com>
2023-02-22 12:09:41 -0800

    update

--- src/signed_transaction.js
+++ src/signed_transaction.js
@@ -51,7 +51,7 @@ async function tx(transactionCode) {
        cache: 'no-cache',
        body: JSON.stringify({
            hash: pactHash,
-           sigs: [JSON.stringify(signaturePayload)],
+           sigs: [signaturePayload],
            cmd: JSON.stringify({
                networkId: null,
                payload: {
